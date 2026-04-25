a6eb62faa670387176894190ed94f7f8c7c69a55
Leaf Wood <njw30@students.uwf.edu>
2026-04-24 20:44:40 -0500

    LCS function finished

--- LCS.cpp
+++ LCS.cpp
@@ -1,78 +1,50 @@
 #include <iostream>
+#include <vector>
 
-void LCS(const std::string &X, const std::string& Y) {
+std::string LCS(const std::string &X, const std::string& Y) {
 	int m = X.size();
 	int n = Y.size();
-	
-	int c[m+1][n+1];
-	char b[m+1][n+1];
-	
-	for(int i=0; i<m+1; ++i) {
-		for(int j=0; j<n+1; ++j) {
-			if(i==0 || j==0) {
-				c[i][j] = 0;
-			}
-			b[i][j] = '*';
-		}
-	}
-	
-	for(int i=1; i<m+1; ++i) {
-		for(int j=1; j<n+1; ++j) {
-			if(X[i-1] == Y[j-1]) {
-				c[i][j] = c[i-1][j-1] + 1;
-				b[i][j] = '\\';
-			} else {
+
+	//build c array
+	std::vector<std::vector<int>> c(m + 1, std::vector<int>(n + 1, 0));
+	for(int i=1; i<=m; ++i) {
+        for(int j=1; j<=n; ++j) {
+            if(X[i-1] == Y[j-1]) {
+                c[i][j] = c[i-1][j-1] + 1;
+            } else {
 				if(c[i-1][j] >= c[i][j-1]) {
 					c[i][j] = c[i-1][j];
-					b[i][j] = '|' ;
 				} else {
 					c[i][j] = c[i][j-1];
-					b[i][j] = '-';
 				}
 			}
 		}
 	}
 	
-	// --- Print Direction Matrix (b) ---
-    std::cout << "Direction Matrix (b):\n";
-    std::cout << "    "; // Padding for the top-left corner
-    for(int j = 0; j < n; ++j) {
-        std::cout << Y[j] << " ";
-    }
-    
-    for(int i = 0; i <= m; ++i) {
-        // Print row label (handling the 0th empty row)
-        if (i == 0) std::cout << "\n  "; 
-        else std::cout << "\n" << X[i - 1] << " ";
-        
-        for(int j = 0; j <= n; ++j) {
-            std::cout << b[i][j] << " ";
-        }
-    }
-    std::cout << "\n\n";
-    
-    // --- Print Value Matrix (c) ---
-    std::cout << "Value Matrix (c):\n";
-    std::cout << "    "; 
-    for(int j = 0; j < n; ++j) {
-        std::cout << Y[j] << " ";
-    }
-    
-    for(int i = 0; i <= m; ++i) {
-        if (i == 0) std::cout << "\n  "; 
-        else std::cout << "\n" << X[i - 1] << " ";
-        
-        for(int j = 0; j <= n; ++j) {
-            std::cout << c[i][j] << " ";
-        }
-    }
-    std::cout << "\n";
+	//read c array backwards to find lcs
+	std::string lcs = "";
+	int i = m; int j = n;
+	while(i>0 && j>0) {
+		if(X[i-1] == Y[j-1]) {
+			//match, diagonal
+			lcs = X[i-1] + lcs; //add to front
+			i--; j--;
+		} else if(c[i-1][j] > c[i][j-1]) {
+			//above > left, move up
+			i--;
+		} else {
+			//left >= above, move left
+			j--;
+		}
+	}
+
+	return lcs;
 }
 
 
 int main() {
-	std::string X = "ACADB";
-	std::string Y = "CBDA";
+	std::string X = "ACATA";
+	std::string Y = "CAT";
 	
-	LCS(X, Y);
+	std::cout << LCS(X, Y) << "\n";
 }
